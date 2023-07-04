dbd21f1531d45224b1ad2616aad5d61c1202abea
mmenaka24 <menaka_maruthainar@live.com>
2023-07-04 14:57:47 +0100

    Added 'Fezz' for multiples of 13

--- index.ts
+++ index.ts
@@ -3,26 +3,35 @@ console.log('FizzBuzz project.......')
 //Instead, set variables to for each fizz, buzz, bang etc...
 //as well as whether or not to print number
 
-for (let i = 1; i < 101; i++) {
-    if (i%11 === 0) {
-        console.log("Bong")
-    } else {
-        let three = ""
-        let five = ""
-        let seven = ""
-        let printnum = i.toString()
-        if (i%3 === 0) {
-            three = "Fizz"
-            printnum = ""
-        }
-        if (i%5 === 0) {
-            five = "Buzz"
-            printnum = ""
-        }
-        if (i%7 === 0) {
-            seven = "Bang"
-            printnum = ""
-        }
-        console.log(printnum + three + five + seven)
+for (let i = 1; i < 201; i++) {
+    let three = "";
+    let five = "";
+    let seven = "";
+    let eleven = "";
+    let thirteen = "";
+    let printnum = i.toString();
+    if (i%3 === 0) {
+        three = "Fizz";
+        printnum = "";
     }
-}
+    if (i%13 === 0) {
+        thirteen = "Fezz";
+        printnum = "";
+    }
+    if (i%5 === 0) {
+        five = "Buzz";
+        printnum = "";
+    }
+    if (i%7 === 0) {
+        seven = "Bang";
+        printnum = "";
+    }
+    if (i%11 === 0) {
+        three = "";
+        five = "";
+        seven = "";
+        eleven = "Bong";
+        printnum = "";
+    }
+    console.log(printnum + three + thirteen + five + seven + eleven)
+}    
